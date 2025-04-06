555560a37440e3cf8f3b977c178e04a167aa28ec
Wildnyker <sgrynchuk@outlook.com>
2025-04-06 13:15:26 +0200

    token

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request} from '@playwright/test';
 // Import mocked tags data from a JSON file - and gave it name of tags
 import tags from '../tests/test-data/test-data.json'
+
 
 test.beforeEach(async ({page})=>{
   // Set up an API mock for the /api/tags endpoint (request)
@@ -14,6 +15,14 @@ test.beforeEach(async ({page})=>{
     })
   })
   
+
+  await page.goto('https://conduit.bondaracademy.com/')
+  // Wait briefly to allow the page and mocked API data to load/render
+  await page.waitForTimeout(500)
+})
+
+test('has title', async ({ page }) => {
+
   //modifying response
   //nortice that i used witdcards to excahage for full api link
   await page.route('*/**/api/articles*', async route=>{
@@ -28,17 +37,32 @@ test.beforeEach(async ({page})=>{
       body: JSON.stringify(responseBody)
     })
   })
-
-  await page.goto('https://conduit.bondaracademy.com/')
-  // Wait briefly to allow the page and mocked API data to load/render
-  await page.waitForTimeout(2000)
-})
-
-test('has title', async ({ page }) => {
+  //it's to refresh the page so the mock will work
+  await page.getByText('Global Feed').click()
 
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
 
   expect (page.locator('app-article-list h1').first()).toContainText("This is a test title")
   expect (page.locator('app-article-list p').first()).toContainText("This is me mocking the description")
+  await page.waitForTimeout(1000)
 });
+
+
+//added request to read it within a test
+test('delete article', async ({page, request})=>{
+  //makinf api request
+  // it should be exported from @playwright/test
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login',{
+    //data is request body
+    data:{
+      "user":{"email":"wild99@test.com","password":"wild99"}
+    }
+  })
+  //saving response as json
+  const responseBody = await response.json()
+  //storing token
+  const accessToken = responseBody.user.token
+  //console.log(responseBody.user.token)
+
+})
 
